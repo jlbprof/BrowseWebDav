af4d5c31c986d2f45f4700e01ec8072bfb415253
Julian Brown <julian@jlbprof.com>
2018-04-28 10:18:28 -0400

    Minor fixes on original example_config

--- example_config.js
+++ example_config.js
@@ -1,6 +1,6 @@
 
-var config_username = 'XXXXX';
-var config_password = 'YYYYY',
-var config_port = 8008;
-var config_url = 'mynextcloud.tld:' + config_port + '/nextcloud/remote.php/dav/files/' + username;
+var config_username = 'XXXXXX';
+var config_password = 'YYYYYY';
+var config_port = 1234;
+var config_url = 'mynextcloud.tld:' + config_port + '/nextcloud/remote.php/dav/files/' + conifg_username;
 
